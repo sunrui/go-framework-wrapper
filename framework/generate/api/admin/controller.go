3a61c4285a3dff5ac88c00db33f4f23a68776d2b
sunrui <sunrui@daliangqing.com>
2022-04-07 16:31:03 +0800

    1、移除掉的模板项目。

--- framework/generate/api/admin/controller.go
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) 2022 honeysense.com All rights reserved.
- * Author: sunrui
- * Date: 2022-04-07 10:58:32
- */
-
-package admin
--- framework/generate/api/user/controller.go
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) 2022 honeysense.com All rights reserved.
- * Author: sunrui
- * Date: 2022-04-07 11:00:59
- */
-
-package user
