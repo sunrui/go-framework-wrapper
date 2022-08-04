86795e910181a3d778df0e7c9c035e3d6f293733
sunrui <smallrui@foxmail.com>
2022-08-04 23:52:10 +0800

    1、在返回结果中去掉 messageType 类型。（客户端收到报错以后，自动 dialog 统一）

--- framework/proto/result/result.go
+++ framework/proto/result/result.go
@@ -11,15 +11,6 @@ import (
 	"framework/proto/request"
 )
 
-// MessageType 消息类型
-type MessageType string
-
-const (
-	MessageTypeToast  MessageType = "Toast"  // 消息提示
-	MessageTypeDialog MessageType = "Dialog" // 消息弹窗
-	MessageTypeIgnore MessageType = "Ignore" // 消息忽略
-)
-
 // Pagination 分页对象
 type Pagination struct {
 	Page      int   `json:"page"`      // 当前页，从 1 开始
@@ -30,23 +21,16 @@ type Pagination struct {
 
 // Result 结果对象
 type Result struct {
-	Code        string           `json:"code" example:"Ok"`           // 结果
-	Message     string           `json:"message" example:"成功"`        // 消息
-	MessageType MessageType      `json:"messageType" example:"Toast"` // 消息类型
-	Data        any              `json:"data,omitempty"`              // 数据
-	Pagination  *Pagination      `json:"pagination,omitempty"`        // 分页对象
-	Request     *request.Request `json:"request,omitempty"`           // 请求对象
+	Code       string           `json:"code" example:"Ok"`    // 结果
+	Message    string           `json:"message" example:"成功"` // 消息
+	Data       any              `json:"data,omitempty"`       // 数据
+	Pagination *Pagination      `json:"pagination,omitempty"` // 分页对象
+	Request    *request.Request `json:"request,omitempty"`    // 请求对象
 }
 
 // WithMessage 设置消息
 func (result Result) WithMessage(message string) Result {
 	result.Message = message
-	return result
-}
-
-// WithMessageType 设置消息类型
-func (result Result) WithMessageType(messageType MessageType) Result {
-	result.MessageType = messageType
 	return result
 }
 
@@ -97,28 +81,27 @@ func (result Result) String(format bool) string {
 
 // 通用返回对象码
 var (
-	Ok                     = newResult("Ok", "成功", MessageTypeToast)
-	NoAuth                 = newResult("NoAuth", "没有登录", MessageTypeIgnore)
-	ParameterBindError     = newResult("ParameterBindError", "参数绑定错误", MessageTypeDialog)
-	ParameterValidateError = newResult("ParameterValidateError", "参数较验错误", MessageTypeDialog)
-	Forbidden              = newResult("Forbidden", "没有权限", MessageTypeDialog)
-	NotFound               = newResult("NotFound", "不存在", MessageTypeDialog)
-	NoMatch                = newResult("NoMatch", "不匹配", MessageTypeDialog)
-	NoContent              = newResult("NoContent", "没有数据", MessageTypeDialog)
-	MethodNotAllowed       = newResult("MethodNotAllowed", "请求方式不允许", MessageTypeDialog)
-	Conflict               = newResult("Conflict", "请求冲突", MessageTypeDialog)
-	RateLimit              = newResult("RateLimit", "限流", MessageTypeDialog)
-	InternalError          = newResult("InternalError", "内部错误", MessageTypeDialog)
-	ThirdPartyError        = newResult("ThirdPartyError", "第三方错误", MessageTypeDialog)
-	NotImplemented         = newResult("NotImplemented", "未实现", MessageTypeDialog)
+	Ok                     = newResult("Ok", "成功")
+	NoAuth                 = newResult("NoAuth", "没有登录")
+	ParameterBindError     = newResult("ParameterBindError", "参数绑定错误")
+	ParameterValidateError = newResult("ParameterValidateError", "参数较验错误")
+	Forbidden              = newResult("Forbidden", "没有权限")
+	NotFound               = newResult("NotFound", "不存在")
+	NoMatch                = newResult("NoMatch", "不匹配")
+	NoContent              = newResult("NoContent", "没有数据")
+	MethodNotAllowed       = newResult("MethodNotAllowed", "请求方式不允许")
+	Conflict               = newResult("Conflict", "请求冲突")
+	RateLimit              = newResult("RateLimit", "限流")
+	InternalError          = newResult("InternalError", "内部错误")
+	ThirdPartyError        = newResult("ThirdPartyError", "第三方错误")
+	NotImplemented         = newResult("NotImplemented", "未实现")
 )
 
 // 创建结果对象
-func newResult(code string, message string, messageType MessageType) Result {
+func newResult(code string, message string) Result {
 	return Result{
-		Code:        code,
-		Message:     message,
-		MessageType: messageType,
+		Code:    code,
+		Message: message,
 	}
 }
 
